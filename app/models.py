75264a5d9d97820bb6ee9930f550e29d03f49ef8
Aditi-005 <aditi.diwan005@gmail.com>
2023-07-28 12:13:49 +0530

    OC-00: Update requirements file.

--- app/models.py
+++ app/models.py
@@ -3,6 +3,26 @@ from sqlalchemy.orm import validates, relationship
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
+from sqlalchemy.orm import composite
+
+
+class Companies(Base):
+    __tablename__ = "companies"
+    company_id = Column(BIGINT, nullable=False, primary_key=True, autoincrement=True)
+    user_contact = Column(BIGINT, ForeignKey(
+        "customers.customer_contact", ondelete="CASCADE"), nullable=False)
+    company_name = Column(String, nullable=False)
+    password = Column(String, primary_key=True, nullable=False)
+    email = Column(String, nullable=True)
+    company_contact = Column(BIGINT, nullable=True)
+    company_address = Column(JSON, nullable=False)
+
+    @validates('company_name', 'password', 'email', 'company_contact', 'company_address')
+    def empty_string_to_null(self, key, value):
+        if isinstance(value, str) and value == '':
+            return None
+        else:
+            return value
 
 
 class User(Base):
@@ -11,11 +31,39 @@ class User(Base):
     customer_name = Column(String, nullable=False)
     customer_contact = Column(BIGINT, primary_key=True, nullable=False)
     customer_birthdate = Column(Date, nullable=True)
-    companies = Column(JSON, nullable=True)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
 
     @validates('customer_name', 'customer_contact', 'customer_id')
+    def empty_string_to_null(self, key, value):
+        if isinstance(value, str) and value == '':
+            return None
+        else:
+            return value
+
+
+class CompositeKey:
+    def __init__(self, company_id, user_contact):
+        self.company_id = company_id
+        self.user_contact = user_contact
+
+    def __composite_values__(self):
+        return self.company_id, self.company_id
+
+
+class UserCompany(Base):
+    __tablename__ = "user-companies"
+
+    company_id = Column(String, ForeignKey(
+        "companies.company_id", ondelete="CASCADE"), nullable=False)
+    user_contact = Column(BIGINT, ForeignKey(
+        "customers.customer_contact", ondelete="CASCADE"), nullable=False)
+    composite_key = composite(CompositeKey, company_id, user_contact)
+
+    customer = relationship("User")
+    company = relationship("Companies")
+
+    @validates('company_id', 'user_contact')
     def empty_string_to_null(self, key, value):
         if isinstance(value, str) and value == '':
             return None
@@ -69,9 +117,9 @@ class Bookings(Base):
 
     booking_id = Column(BIGINT, nullable=False, primary_key=True, autoincrement=True)
     user_contact = Column(BIGINT, ForeignKey(
-        "customers.customer_contact", ondelete="CASCADE"), nullable=False,)
+        "customers.customer_contact", ondelete="CASCADE"), nullable=False, )
     company_id = Column(String, nullable=False)
-    products = Column(JSON, nullable=False )
+    products = Column(JSON, nullable=False)
     address_id = Column(BIGINT, ForeignKey(
         "address.address_id", ondelete="CASCADE"), nullable=False)
     booking_date = Column(Date, nullable=False)
