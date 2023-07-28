aee591f1ccc96b98a4b2313ca1c41b639c79e798
Aditi-005 <aditi.diwan005@gmail.com>
2023-07-28 13:36:04 +0530

    OC-00: Fix Render Database link.

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Mun1chad$@localhost:5432/onecart"
+SQLALCHEMY_DATABASE_URL = "postgres://onecart_database_user:YXJ7uE6WPhN4fNVVnIRxUS7Gi7p3m5V8@dpg-civsov407spr6tan2e40-a.oregon-postgres.render.com/onecart_database"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
