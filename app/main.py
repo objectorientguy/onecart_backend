338a8a9a1979a90711c5948ac3a1da3f4c2ec922
Aakash-20 <skyakash480@gmail.com>
2023-08-19 11:00:02 +0530

    Master : Created products Search API

--- app/main.py
+++ app/main.py
@@ -618,14 +618,14 @@ def delete_multiple_products(product_ids: List[int], response: Response, db: Ses
         response.status_code = 500
         return {"status": "500", "message": "Internal server error"}
 
-@app.get("/products/search")
+@app.get("/productsSearch")
 def search_products(response: Response, product_name: str, db: Session = Depends(get_db)):
     try:
-        fetch_products = db.query(models.Products).filter(models.Products.product_name.like(f"%{product_name}%")).all()
-        if not fetch_products:
+        search_product = db.query(models.Products).filter(models.Products.product_name.like(f"%{product_name}%")).all()
+        if not search_product:
             return {"status": 204, "message": "No product found", "data": {}}
 
-        return {"status": 200, "message": "Products fetched", "data": fetch_products}
+        return {"status": 200, "message": "Products fetched", "data": search_product}
     except IntegrityError as e:
         print(repr(e))
         response.status_code = 200
