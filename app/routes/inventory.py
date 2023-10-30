46a3b48ccd0967d1864bc1e8f592808a21525256
Aakash-20 <skyakash480@gmail.com>
2023-10-30 18:32:48 +0530

    dev: debugged the fetch stock api

--- app/routes/inventory.py
+++ app/routes/inventory.py
@@ -5,6 +5,36 @@ from app.database import get_db
 router = APIRouter()
 
 
+@router.get("/stock/barcode")
+def fetch_product_details(barcode_no: int, db: Session = Depends(get_db)):
+    try:
+        stock = db.query(models.Stock).filter(models.Stock.barcode_no == barcode_no).first()
+        if not stock:
+            return {"status": 404, "message": "Variant not found!", "data": {}}
+
+        variant = db.query(models.ProductVariant).filter(models.ProductVariant.variant_id == stock.variant_id).first()
+        product = db.query(models.Products).filter(models.Products.product_id == stock.product_id).first()
+        stock_data = {
+            "stock_id": stock.stock_id,
+            "product_id": stock.product_id,
+            "product_name": product.product_name,
+            "product_image": variant.image,
+            "stock": variant.stock,
+            "qt_unit": str(variant.quantity) + " " + variant.measuring_unit,
+            "selling_price": variant.discounted_cost,
+            "variant_id": stock.variant_id,
+            "current_stock_count": stock.current_stock_count,
+            "barcode_no": stock.barcode_no,
+            "reorder_stock_at": stock.reorder_stock_at if stock.reorder_stock_at else 0,
+            "perishable": stock.perishable if stock.perishable else ""
+        }
+        return {"status": 200, "message": "Product details fetched successfully!",
+                "data": stock_data}
+    except Exception as e:
+        print(repr(e))
+        return {"status": 500, "message": "Internal Server Error!", "data": {}}
+
+
 @router.get("/stock")
 def fetch_stock(db: Session = Depends(get_db)):
     try:
@@ -15,14 +45,21 @@ def fetch_stock(db: Session = Depends(get_db)):
         for item in stock:
             variant = db.query(models.ProductVariant).filter(models.ProductVariant.variant_id == item.variant_id).first()
             product = db.query(models.Products).filter(models.Products.product_id == item.product_id).first()
+            if product is not None:  # Check if product is not None
+                product_name = product.product_name
+            if variant is not None:  # Check if variant is not None
+                product_image = variant.image
+                selling_price = variant.discounted_cost
+                qt_unit = str(variant.quantity) + " " + variant.measuring_unit
+                stock_quantity = variant.stock if variant.stock is not None else 0
             stock_data.append({
                 "stock_id": item.stock_id,
                 "product_id": item.product_id,
-                "product_name": product.product_name,
-                "product_image": variant.image,
-                "stock": variant.stock,
-                "qt_unit": str(variant.quantity) + " " + variant.measuring_unit,
-                "selling_price": variant.discounted_cost,
+                "product_name": product_name,
+                "product_image": product_image,
+                "stock": stock_quantity,
+                "qt_unit": qt_unit,
+                "selling_price": selling_price,
                 "variant_id": item.variant_id,
                 "current_stock_count": item.current_stock_count,
                 "barcode_no": item.barcode_no,
@@ -33,26 +70,6 @@ def fetch_stock(db: Session = Depends(get_db)):
     except Exception as e:
         print(repr(e))
         return {"status": 500, "message": "Internal Server Error", "data": {}}
-
-
-@router.get("/barcode/product-name")
-def fetch_product_details(barcode_no: int, db: Session = Depends(get_db)):
-    try:
-        product_variant = db.query(models.ProductVariant).filter(models.ProductVariant.barcode_no == barcode_no).first()
-        if not product_variant:
-            return {"status": 404, "message": "Variant not found!", "data": {}}
-        product_id = product_variant.product_id
-        variant_id = product_variant.variant_id
-        product = db.query(models.Products).filter_by(models.Products.product_id == product_id).first()
-        if not product:
-            return {"status": 404, "message": "Product not found!", "data": {}}
-        product_name = product.product_name
-        return {"status": 200, "message": "Product details fetched successfully!",
-                "data": {"product_name": product_name, "product_id": product_id,
-                         "variant_id": variant_id, "barcode": barcode_no}}
-    except Exception as e:
-        print(repr(e))
-        return {"status": 500, "message": "Internal Server Error!", "data": {}}
 
 
 @router.get("/get/stock-history")
